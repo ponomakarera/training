81cfcec235b57014b69d5bc131d8876f28c8a3c1
ponomakarera <nidanmawashigeri@gmail.com>
2014-09-23 20:54:00 +0900

    Update skech.js

--- skech.js
+++ skech.js
@@ -89,8 +89,17 @@ var __slice = Array.prototype.slice;
 
 
 
-          if ($(this).attr('data-setBaseImageURL')) {
+          if ($(this).attr('data-download')) {
+            sketch.download($(this).attr('data-download'));
+          }
+          else if ($(this).attr('data-setBaseImageURL')) {
             sketch.setBaseImageURL($(this).attr('data-setBaseImageURL'));
+          }
+          else if ($(this).attr('data-setBgcolor')) {
+            sketch.setBgcolor($(this).attr('data-setBgcolor'));
+          }
+          else if ($(this).attr('data-clear')) {
+            sketch.clear($(this).attr('data-clear'));
           }
 
           return false;
@@ -105,7 +114,7 @@ var __slice = Array.prototype.slice;
 
 
 		// 追加
-    Sketch.prototype.clear = function() {
+    Sketch.prototype.clear = function(format) {
 			var cc = this.context;
 			cc.setTransform(1, 0, 0, 1, 0, 0);
 			cc.clearRect(0, 0, 1000, 1000);
