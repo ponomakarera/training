d7cad6215e5e62a8577cea50e1f8c8c2e17a4602
ponomakarera <nidanmawashigeri@gmail.com>
2014-09-25 02:19:55 +0900

    Update sketch_v2.js

--- sketch/sketch_v2.js
+++ sketch/sketch_v2.js
@@ -356,6 +356,44 @@ var __slice = Array.prototype.slice;
 
     }
   };
+  $.sketch.tools.line = {
+      onEvent: function(e) {
+        var newPoint;
+        switch (e.type) {
+          case 'mousedown':
+          case 'touchstart':
+            newPoint = {
+              x: e.pageX - this.canvas.offset().left,
+              y: e.pageY - this.canvas.offset().top,
+              event: e.type
+            };
+            if (this.point) {
+              this.newAction();
+              this.action.events.push(this.point);
+              this.action.events.push(newPoint);
+              this.actions.push(this.action);
+            }
+            this.point = newPoint;
+            return this.redraw();
+        }
+      },
+      draw: function(action) {
+        var event, previous, _i, _len, _ref;
+        this.context.lineJoin = "round";
+        this.context.lineCap = "round";
+        this.context.beginPath();
+        this.context.moveTo(action.events[0].x, action.events[0].y);
+        _ref = action.events;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          event = _ref[_i];
+          this.context.lineTo(event.x, event.y);
+          previous = event;
+        }
+        this.context.strokeStyle = action.color;
+        this.context.lineWidth = action.size;
+        return this.context.stroke();
+      }
+    };
 
 
 
