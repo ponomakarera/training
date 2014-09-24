c0245463bdfa8ae3763dc85162cb9e5b749cd050
ponomakarera <nidanmawashigeri@gmail.com>
2014-09-24 20:20:30 +0900

    Update sketch_v2.js

--- sketch/sketch_v2.js
+++ sketch/sketch_v2.js
@@ -48,6 +48,7 @@ var __slice = Array.prototype.slice;
       	for (var i in data.response.posts) {
       		for (var j in data.response.posts[i].photos) {
       			this.imageUrl.push(data.response.posts[i].photos[j].original_size.url);
+      			console.log(data.response.posts[i].photos[j].original_size.url);
       		        this.maxphotos++;
       		}
       	}
