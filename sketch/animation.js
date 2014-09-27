2898a0f4d0e38f573b3bfbd32955ea91b821c7f8
ponomakarera <nidanmawashigeri@gmail.com>
2014-09-27 22:14:27 +0900

    Update animation.js

--- sketch/animation.js
+++ sketch/animation.js
@@ -48,7 +48,7 @@ var __slice = Array.prototype.slice;
       $.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/photo?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=画像&limit=50&jsonp=?",function(data) {
       	for (var i in data.response.posts) {
       		for (var j in data.response.posts[i].photos) {
-      			console.log(data.response.posts[i].photos[j].original_size.url);
+      			console.log(data.response.posts[i].photos[j].alt_sizes[4].url);
       			imageUrl.push(data.response.posts[i].photos[j].original_size.url);
       			maxphotos++;
       		}
