f6a01c69de70327d9f622355f2582e57d9d304a7
ponomakarera <nidanmawashigeri@gmail.com>
2015-06-29 13:57:45 +0900

    Update and rename main.js to start.js

---
+++ game/start.js
@@ -0,0 +1,71 @@
+var SCREEN_WIDTH = 800;
+var SCREEN_HEIGHT = 600;
+
+window.addEventListener('load', init);
+
+var canvas;
+var ctx;
+
+function init() {
+  canvas = document.getElementById('maincanvas');
+  ctx = canvas.getContext('2d');
+
+  canvas.width = SCREEN_WIDTH;
+  canvas.height = SCREEN_HEIGHT;
+
+  requestAnimationFrame(update);
+}
+
+function update() {
+  requestAnimationFrame(update);
+
+  render();
+}
+
+function render() {
+  // 全体をクリア
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+var Asset = {}
+
+// アセットの定義
+Asset.assets = [
+  { type: 'image', name: 'back', src: 'assets/back.png' },
+  { type: 'image', name: 'box', src: 'assets/box.png' }
+];
+
+// 読み込んだ画像
+Asset.images = {};
+
+// アセットの読み込み
+Asset.loadAssets = function(onComplete) {
+  var total = Asset.assets.length; // アセットの合計数
+  var loadCount = 0; // 読み込み完了したアセット数
+
+  // アセットが読み込み終わった時に呼ばれるコールバック関数
+  var onLoad = function() {
+    loadCount++; // 読み込み完了数を1つ足す
+    if (loadCount >= total) {
+      // すべてのアセットの読み込みが終わった
+      onComplete();
+    }
+  };
+
+  // すべてのアセットを読み込む
+  Asset.assets.forEach(function(asset) {
+    switch (asset.type) {
+      case 'image':
+        Asset._loadImage(asset, onLoad);
+        break;
+    }
+  });
+};
+
+// 画像の読み込み
+Asset._loadImage = function(asset, onLoad) {
+  var image = new Image();
+  image.src = asset.src;
+  image.onload = onLoad;
+  Asset.images[asset.name] = image;
+};
