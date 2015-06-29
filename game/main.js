d1c9dea636f29b09d0e3b5c725449042bd796227
ponomakarera <nidanmawashigeri@gmail.com>
2015-06-29 13:46:19 +0900

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,4 +1,28 @@
-function main() {}
-main.prototype.print = function () {
-document.write("Hello World!!");
+var SCREEN_WIDTH = 800;
+var SCREEN_HEIGHT = 600;
+
+window.addEventListener('load', init);
+
+var canvas;
+var ctx;
+
+function init() {
+  canvas = document.getElementById('maincanvas');
+  ctx = canvas.getContext('2d');
+
+  canvas.width = SCREEN_WIDTH;
+  canvas.height = SCREEN_HEIGHT;
+
+  requestAnimationFrame(update);
+}
+
+function update() {
+  requestAnimationFrame(update);
+
+  render();
+}
+
+function render() {
+  // 全体をクリア
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
