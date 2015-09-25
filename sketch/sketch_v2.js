a1ae28aba4c8820bccc3a7a5d183768ba58b815b
ponomakarera <nidanmawashigeri@gmail.com>
2015-09-26 04:03:31 +0900

    Update sketch_v2.js

--- sketch/sketch_v2.js
+++ sketch/sketch_v2.js
@@ -45,7 +45,7 @@ var __slice = Array.prototype.slice;
       this.context.fillRect(0, 0, this.canvas.width(), this.canvas.height());
       
       
-      $.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/photo?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=スケッチ&limit=50&jsonp=?",function(data) {
+      $.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/photo?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=スケッチ&limit=20&jsonp=?",function(data) {
       	for (var i in data.response.posts) {
       		for (var j in data.response.posts[i].photos) {
       			console.log(data.response.posts[i].photos[j].original_size.url);
