24fadc0d50efecdce3575c62f63d9c15ce36650f
ponomakarera <nidanmawashigeri@gmail.com>
2015-09-25 15:54:30 +0900

    Update addlink1.1.js

--- addlink1.1.js
+++ addlink1.1.js
@@ -2,7 +2,7 @@ $.fn.addlink = function(baseURL){
     var dict = [];
 				$.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=50&jsonp=?",function(data) {
       	for (var i in data.response.posts) {
-      	  dict.push(data.response.posts[i].slug);
+      	  dict.push(data.response.posts[i].title);
       	  dict.push(data.response.posts[i].post_url);
       	}
       });
