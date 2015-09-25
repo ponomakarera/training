214e46340cbb61249b331c5e6273ef487edfcee1
ponomakarera <nidanmawashigeri@gmail.com>
2015-09-25 20:19:57 +0900

    Update and rename autolink2.8.js to autolink2.9.js

--- autolink2.9.js
+++ autolink2.9.js
@@ -3,7 +3,7 @@ $.fn.addlink = function(baseURL){
   $.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=50&jsonp=?",function(data) {
       	for (var i in data.response.posts) {
       	  dict[data.response.posts[i].title] = data.response.posts[i].post_url;
-      	  console.log("hello");
+      	  console.log(i);
       	}
       });
     return this.each(function(){
