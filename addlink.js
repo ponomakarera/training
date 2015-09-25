2512d9a5339f02b20d4d1918d512a682dcf0d933
ponomakarera <nidanmawashigeri@gmail.com>
2015-09-25 14:18:47 +0900

    Update addlink.js

--- addlink.js
+++ addlink.js
@@ -3,6 +3,7 @@ $.fn.addlink = function(baseURL){
                 "京都","http://ja.wikipedia.org/wiki/京都市",
                 "秋葉原","http://ja.wikipedia.org/wiki/秋葉原",
                 "築地市場","http://ja.wikipedia.org/wiki/築地市場", 
+                "テスト","http://ponomakarera.tumblr.com/post/129829397015/%E3%83%86%E3%82%B9%E3%83%88",
 				];
     return this.each(function(){
         var srcText = this.innerHTML;
