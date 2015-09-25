fde4fca60cd430267a87d072e3866c452640fb5d
ponomakarera <nidanmawashigeri@gmail.com>
2015-09-25 20:04:20 +0900

    Update and rename autolink2.7.js to autolink2.8.js

--- autolink2.8.js
+++ autolink2.8.js
@@ -1,5 +1,5 @@
 $.fn.addlink = function(baseURL){
-  var dict = {};
+  var dict = {test:"http://ponomakarera.tumblr.com/post/129834164555/test"};
   $.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=50&jsonp=?",function(data) {
       	for (var i in data.response.posts) {
       	  dict[data.response.posts[i].title] = data.response.posts[i].post_url;
@@ -11,7 +11,7 @@ $.fn.addlink = function(baseURL){
         console.log("start");
         for (var i in dict){
         console.log("running");
-            srcText = srcText.replace(new RegExp(dict[i], "g"),"<a href='"+dict[i]+"'>"+i+"</a>");
+            srcText = srcText.replace(new RegExp(i, "g"),"<a href='"+dict[i]+"'>"+i+"</a>");
             
         }
         this.innerHTML = srcText;
