d08470b5a31cdaf99ff301129b0d73512448467e
ponomakarera <nidanmawashigeri@gmail.com>
2015-09-25 14:34:58 +0900

    Update addlink.js

--- addlink.js
+++ addlink.js
@@ -1,5 +1,5 @@
 $.fn.addlink = function(baseURL){
-    var dict = ["富士山", "http://ja.wikipedia.org/wiki/富士山",
+    var dict = ["富士山", "http://ponomakarera.tumblr.com/post/129829397015/%E3%83%86%E3%82%B9%E3%83%88",
                 "京都","http://ja.wikipedia.org/wiki/京都市",
                 "秋葉原","http://ja.wikipedia.org/wiki/秋葉原",
                 "築地市場","http://ja.wikipedia.org/wiki/築地市場", 
