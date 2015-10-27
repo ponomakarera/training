ba66912f70d1165e5878637bc3fe2e91ec0262d5
ponomakarera <nidanmawashigeri@gmail.com>
2015-10-27 11:21:09 +0900

    Create addlink3.6.js

---
+++ addlink/addlink3.6.js
@@ -0,0 +1,26 @@
+$(function () {
+    var total_posts = 0;
+    $.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=20&jsonp=?", function (data) {
+        total_posts = data.response.total_posts;
+    });
+    
+    for (var i = 0; i <= total_posts / 20; i++) {
+    $.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=20&offset="+ i * 20 +"&jsonp=?",function(data) {
+        $(".addlink").each(function(){
+		for (var j in data.response.posts) {
+			var linkbody = data.response.posts[j].body;
+			linkbody = linkbody.replace(/"/g,'&quot;');
+			linkbody = linkbody.replace(/'/g,'&#39;');
+			linkbody = linkbody.replace(/&/g,'&amp;');
+			linkbody = linkbody.replace(/>/g,'&gt;');
+			linkbody = linkbody.replace(/</g,'&lt;');
+		    var txt = $(this).html();
+		$(this).html(txt.replace(new RegExp(data.response.posts[j].title),"<a  class='textlink' title='"+ linkbody +"'; href='"+data.response.posts[j].post_url+"' style='color:#0645ad '>"+data.response.posts[j].title+"</a>"));}
+	});
+    	$(document).ready(function(){
+	 simple_tooltip(".textlink","tooltip");
+});
+    }
+      );
+    }
+});
