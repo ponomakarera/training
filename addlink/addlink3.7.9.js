7c2e9161ce5aa3051ea23504520582c0a3b22c93
ponomakarera <nidanmawashigeri@gmail.com>
2015-10-27 15:26:48 +0900

    Update and rename addlink3.7.8.js to addlink3.7.9.js

--- addlink/addlink3.7.9.js
+++ addlink/addlink3.7.9.js
@@ -4,21 +4,19 @@ $(function () {
         total_posts = data.response.total_posts;
     });
     
-    var name = "wordlist";
-    var number = 0;
+    var wordlist = "wordlist";
     for (var i = 0; i <= total_posts / 20; i++) {
     $.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=20&offset="+ i * 20 +"&jsonp=?",function(data) {
         $(".addlink").each(function(){
 		for (var i in data.response.posts) {
 		    var txt = $(this).html();
-		    $(this).html(txt.replace(new RegExp(data.response.posts[i].title),"<span id='wordlist'>"+data.response.posts[i].title+"</span>"));
+		    $(this).html(txt.replace(new RegExp(data.response.posts[i].title),wordlist+data.response.posts[i].title));
 		}
 	});
     }
       );
     }
     
-    number = 1;
     for (var i = 0; i <= total_posts / 20; i++) {
     $.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=20&offset="+ i * 20 +"&jsonp=?",function(data) {
 	$(".addlink").each(function() {
@@ -30,7 +28,7 @@ $(function () {
 			linkbody = linkbody.replace(/>/g,'&gt;');
 			linkbody = linkbody.replace(/</g,'&lt;');
 		    var txt = $(this).html();
-		    $(this).html(txt.replace(new RegExp("<span id='wordlist'>"+data.response.posts[i].title+"</span>"),"<a  class='textlink' title='"+ linkbody +"'; href='"+data.response.posts[i].post_url+"' style='color:#0645ad '>"+data.response.posts[i].title+"</a>"));
+		    $(this).html(txt.replace(new RegExp(wordlist+data.response.posts[i].title),"<a  class='textlink' title='"+ linkbody +"'; href='"+data.response.posts[i].post_url+"' style='color:#0645ad '>"+data.response.posts[i].title+"</a>"));
 		}
 	});
     	$(document).ready(function(){
