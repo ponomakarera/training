5c6b2e75df7801bcaca0d568ecdff36c3ef750b4
ponomakarera <nidanmawashigeri@gmail.com>
2015-11-14 19:04:53 +0900

    Update and rename Title1.js to Title2.js

--- game/shooting/Title2.js
+++ game/shooting/Title2.js
@@ -3,7 +3,7 @@ this.cours = 0;
 }
 
 Title.prototype.update = function (gameini) {
-gameini.images.push({type:"dxy", src:"http://40.media.tumblr.com/473c1c59e7d97ecf58860c7c2b528868/tumblr_nxsqourU8V1t0jrzao4_400.png", dx:0, dy:0});
+gameini.images.push({type:"dxy", src:"http://41.media.tumblr.com/db5704dae9e2f99cb6a88d7b594708b9/tumblr_nxsqourU8V1t0jrzao4_r1_400.jpg", dx:0, dy:0});
 gameini.images.push({type:"dxy", src:"http://40.media.tumblr.com/9ff982762ea31e0ae39eb1df079ed811/tumblr_nxsqourU8V1t0jrzao5_100.png", dx:60, dy:100});
 if (gameini.key.space()) {
 	gameini.sequence = gameini.seqtype['game'];
