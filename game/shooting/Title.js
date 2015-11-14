92da19e80e76e3715627734fbb66c90c3c7a6952
ponomakarera <nidanmawashigeri@gmail.com>
2015-11-14 17:43:05 +0900

    Update Title.js

--- game/shooting/Title.js
+++ game/shooting/Title.js
@@ -3,8 +3,8 @@ this.cours = 0;
 }
 
 Title.prototype.update = function (gameini) {
-gameini.images.push({type:"dxy", src:"../rsrc/image/title.png", dx:0, dy:0});
-gameini.images.push({type:"dxy", src:"../rsrc/image/press_space.png", dx:60, dy:100});
+gameini.images.push({type:"dxy", src:"http://40.media.tumblr.com/473c1c59e7d97ecf58860c7c2b528868/tumblr_nxsqourU8V1t0jrzao4_400.png", dx:0, dy:0});
+gameini.images.push({type:"dxy", src:"http://40.media.tumblr.com/9ff982762ea31e0ae39eb1df079ed811/tumblr_nxsqourU8V1t0jrzao5_100.png", dx:60, dy:100});
 if (gameini.key.space()) {
 	gameini.sequence = gameini.seqtype['game'];
 }
