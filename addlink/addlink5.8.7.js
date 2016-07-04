24056f0e405b78d6678770b984a4fb2522534d06
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-05 08:47:40 +0900

    Update and rename addlink5.8.6.js to addlink5.8.7.js

--- addlink/addlink5.8.7.js
+++ addlink/addlink5.8.7.js
@@ -6,20 +6,15 @@ $(function () {
 	var url = {};
 	
 	$.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=1&jsonp=?", function (data) {
+			
 			var total_posts = data.response.total_posts;
-			console.log("data.response.total_posts = "+ data.response.total_posts);
-			
 			var count = total_posts / 20;
-			console.log("total_posts = "+ total_posts);
-			console.log("count = "+ count);
-			
 			for (var i = 0; i <= count; i++) {
 				$.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
 					for (var i in data.response.posts) {
 						array[listnumber++] = data.response.posts[i].title;
 						body[data.response.posts[i].title] = data.response.posts[i].body;
 						url[data.response.posts[i].title] = data.response.posts[i].post_url;
-						console.log(array[listnumber - 1]);
 					}
 				});
 			}
