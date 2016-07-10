67149389541b69fbf4e7c04755cc169cdbc94fb8
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-10 23:09:20 +0900

    Update and rename tagsappend.24.js to tagsappend.25.js

--- tooltips/tagsappend.25.js
+++ tooltips/tagsappend.25.js
@@ -14,6 +14,7 @@ $(function () {
 				$.getJSON("http://api.tumblr.com/v2/blog/tagmani.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
 					for (var i = data.response.posts.length - 1; i >= 0; i--) {
 						for (var j in data.response.posts[i].tags) {
+							console.log(data.response.posts[i].tags[j]);
 							if (data.response.posts[i].tags[j].match(/(.+)\.\.(.+)/)) {
 								var major = RegExp.$1;
 								var minor = RegExp.$2;
