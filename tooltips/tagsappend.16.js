f0829e2c26687472c29253ae8cc47e72aa25a11d
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-10 22:21:51 +0900

    Update and rename tagsappend.15.js to tagsappend.16.js

--- tooltips/tagsappend.16.js
+++ tooltips/tagsappend.16.js
@@ -17,7 +17,7 @@ $(function () {
 						for (var j in data.response.posts[i].tags) {
 							if (data.response.posts[i].tags[j].match(match)) {
 								console.log("$1 = "+ RegExp.$1 +" $2 = "+ RegExp.$2);
-								$("#"+ RegExp.$1).append("<li class='closed'><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$2 +"'>$2</a><ul id='"+ RegExp.$2 +"'><li><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$1 +".."+ RegExp.$2 +"'>ローカルルール</a></li></ul></li>");
+								$("#"+ RegExp.$1).append("<li class='closed'><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$2 +"'>"+ RegExp.$2 +"</a><ul id='"+ RegExp.$2 +"'><li><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$1 +".."+ RegExp.$2 +"'>ローカルルール</a></li></ul></li>");
 							}
 						}
 					}
