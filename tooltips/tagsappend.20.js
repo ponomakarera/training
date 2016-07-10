6583fa7458315d5d6b9b14c898fae393af936f9c
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-10 22:41:33 +0900

    Update and rename tagsappend.19.js to tagsappend.20.js

--- tooltips/tagsappend.20.js
+++ tooltips/tagsappend.20.js
@@ -15,9 +15,9 @@ $(function () {
 				$.getJSON("http://api.tumblr.com/v2/blog/tagmani.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
 					for (var i in data.response.posts) {
 						for (var j in data.response.posts[i].tags) {
-							if (data.response.posts[i].tags[j].match(/(.+)\.\.(.+)/)) {
+							if (data.response.posts[i].tags[j].replace(/(.+)\.\.(.+)/)) {
 								console.log('$1 $2');
-								$("#"+ RegExp.$1).append("<li class='closed'><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$1 +"'>"+ RegExp.$1 +"</a><ul id='"+ RegExp.$1 +"'><li><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$2 +".."+ RegExp.$1 +"'>ローカルルール</a></li></ul></li>");
+								$("#$1").append("<li class='closed'><a href='http://tagmani.tumblr.com/tagged/$2'>$2</a><ul id='$2'><li><a href='http://tagmani.tumblr.com/tagged/$1..$2'>ローカルルール</a></li></ul></li>");
 							}
 						}
 					}
