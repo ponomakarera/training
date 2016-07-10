20dff5038e34dd7d2ec5e27e4fb09d03f44751ab
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-10 22:32:18 +0900

    Update and rename tagsappend.18.js to tagsappend.19.js

--- tooltips/tagsappend.19.js
+++ tooltips/tagsappend.19.js
@@ -9,13 +9,13 @@ $(function () {
 			
 			var total_posts = data.response.total_posts;
 			var count = total_posts / 20;
-			var match = /(.+)\.\.(.+)/;
+			//var match = /(.+)\.\.(.+)/;
 			
 			for (var i = 0; i <= count; i++) {
 				$.getJSON("http://api.tumblr.com/v2/blog/tagmani.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
 					for (var i in data.response.posts) {
 						for (var j in data.response.posts[i].tags) {
-							if (data.response.posts[i].tags[j].match(match)) {
+							if (data.response.posts[i].tags[j].match(/(.+)\.\.(.+)/)) {
 								console.log('$1 $2');
 								$("#"+ RegExp.$1).append("<li class='closed'><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$1 +"'>"+ RegExp.$1 +"</a><ul id='"+ RegExp.$1 +"'><li><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$2 +".."+ RegExp.$1 +"'>ローカルルール</a></li></ul></li>");
 							}
