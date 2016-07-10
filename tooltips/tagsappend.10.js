4220a76c9311d1c80bc61cbb36223b13c1ae7a70
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-10 22:02:15 +0900

    Update and rename tagsappend.9.js to tagsappend.10.js

--- tooltips/tagsappend.10.js
+++ tooltips/tagsappend.10.js
@@ -17,6 +17,8 @@ $(function () {
 						for (var j in data.response.posts[i].tags) {
 							if (data.response.posts[i].tags[j].match(match)) {
 								data.response.posts[i].tags[j].replace(match);
+								console.log(data.response.posts[i].tags[j]);
+								console.log("$1..$2");
 								$("#$1").append("<li class='closed'><a href='http://tagmani.tumblr.com/tagged/$2'>$2</a><ul id='$2'><li><a href='http://tagmani.tumblr.com/tagged/$1..$2'>ローカルルール</a></li></ul></li>");
 							}
 						}
