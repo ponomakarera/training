5943355aa7437f87c79c3626707afa67506500ee
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-10 21:03:13 +0900

    Update and rename tagsappend.1.js to tagsappend.2.js

--- tooltips/tagsappend.2.js
+++ tooltips/tagsappend.2.js
@@ -15,7 +15,7 @@ $(function () {
 				$.getJSON("http://api.tumblr.com/v2/blog/tagmani.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
 					for (var i in data.response.posts) {
 						for (var j in data.response.posts[i].tags) {
-							if (data.response.posts[i].tags[j].search(match)) {
+							if (data.response.posts[i].tags[j].match(match)) {
 								$("#$1").append("<li><a href='http://tagmani.tumblr.com/tagged/$2'>$2</a><ul id='$2'><a href='http://tagmani.tumblr.com/tagged/$1-&gt;$2'>ローカルルール</a></ul></li>");
 							}
 						}
