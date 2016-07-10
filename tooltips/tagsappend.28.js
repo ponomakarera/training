5cefb52af35631bdfe207eb5117f0f51b2d38530
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-10 23:17:19 +0900

    Update and rename tagsappend.27.js to tagsappend.28.js

--- tooltips/tagsappend.28.js
+++ tooltips/tagsappend.28.js
@@ -13,6 +13,7 @@ $(function () {
 			for (var i = count; i >= 0; i--) {
 				$.getJSON("http://api.tumblr.com/v2/blog/tagmani.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
 					for (var i = data.response.posts.length - 1; i >= 0; i--) {
+						console.log(data.response.posts.length);
 						for (var j in data.response.posts[i].tags) {
 							console.log(data.response.posts[i].tags[j]);
 							if (data.response.posts[i].tags[j].match(/(.+)\.\.(.+)/)) {
