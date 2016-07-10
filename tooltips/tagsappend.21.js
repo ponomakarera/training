f142489f7d5f277085cdc7c1d16fc1db7572bbd5
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-10 22:46:38 +0900

    Update and rename tagsappend.20.js to tagsappend.21.js

--- tooltips/tagsappend.21.js
+++ tooltips/tagsappend.21.js
@@ -16,8 +16,8 @@ $(function () {
 					for (var i in data.response.posts) {
 						for (var j in data.response.posts[i].tags) {
 							if (data.response.posts[i].tags[j].replace(/(.+)\.\.(.+)/)) {
-								console.log('$1 $2');
-								$("#$1").append("<li class='closed'><a href='http://tagmani.tumblr.com/tagged/$2'>$2</a><ul id='$2'><li><a href='http://tagmani.tumblr.com/tagged/$1..$2'>ローカルルール</a></li></ul></li>");
+								console.log('$1 = '+ RegExp.$1 +' $2 = '+ RegExp.$2);
+								$("#"+ RegExp.$1).append("<li class='closed'><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$2 +"'>"+ RegExp.$2 +"</a><ul id='"+ RegExp.$2 +"'><li><a href='http://tagmani.tumblr.com/tagged/"+ RegExp.$1 +".."+ RegExp.$2 +"'>ローカルルール</a></li></ul></li>");
 							}
 						}
 					}
