d0f741a9fafc4e69398c5144112907b54a32ea95
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-11 14:59:17 +0900

    Update fusianasan.12.js

--- addlink/fusianasan.12.js
+++ addlink/fusianasan.12.js
@@ -1,7 +1,7 @@
 function fusianasan(postid, tag){
 console.log(tag);
 	if (tag.match(/^fusianasan$/)) {
-	console.log("hit);
+	console.log("hit");
 			$("."+ postid +" .author").append("<span>ID:<a href='{PostAuthorURL}'>{PostAuthorName}</a></span>");
 	}
 }
