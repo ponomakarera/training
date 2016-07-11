dc6f4116b54f3d4a49d4f51b8df9b44328ead771
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-11 16:03:12 +0900

    Delete fusianasan.13.js

--- addlink/fusianasan.13.js
+++
@@ -1,7 +0,0 @@
-function fusianasan(postid, title){
-	if (title.match(/^fusianasan$/)) {
-			$("."+ postid +" .author").empty();
-			$("."+ postid +" .author").append("<span>ID:<a href='{PostAuthorURL}'>{PostAuthorName}</a></span>");
-	}
-}
-
