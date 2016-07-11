e36e32edb49dfda85d3509d9c36e7db5716d3389
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-11 13:25:58 +0900

    Update and rename fusianasan.js to fusianasan.1.js

--- addlink/fusianasan.js
+++
@@ -1,5 +0,0 @@
-function fusianasan(postid, fusianasan){
-	if (fusianasan.match(/^fusianasan$/)) {
-		$("."+ postid +" .author").append("<span>ID:<a href="{PostAuthorURL}">{PostAuthorName}</a></span>");
-	}
-}
