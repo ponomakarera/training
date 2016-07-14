a0c67b7f802e216432adbca693697ed47d642181
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-15 02:03:08 +0900

    Update deltreenest.js

--- tooltips/deltreenest.js
+++ tooltips/deltreenest.js
@@ -1,8 +1,9 @@
 funciton deltreenest(major, minor) {
-	for (var key in dic) {
+	
+}
+for (var key in dic) {
 											if (key.match(new RegExp(minor +"→(.+)"))) {
 												deltreenest(minor, RegExp.$1);
 											}
 											delete dic[major +"→"+ minor];
 										}
-}
