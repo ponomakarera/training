4fc5d6a3078407d31e4628f2600d60293046fe62
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-15 01:53:43 +0900

    Create deltreenest.js

---
+++ tooltips/deltreenest.js
@@ -0,0 +1,8 @@
+funciton deltreenest(major, minor) {
+	for (var key in dic) {
+											if (key.match(new RegExp(minor +"→(.+)"))) {
+												deltreenest(minor, RegExp.$1);
+											}
+											delete dic[major +"→"+ minor];
+										}
+}
