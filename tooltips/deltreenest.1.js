9343d5227fa47d03ec348f284d3f353d49ecfc42
ponomakarera <nidanmawashigeri@gmail.com>
2016-07-15 02:04:08 +0900

    Update and rename deltreenest.js to deltreenest.1.js

--- tooltips/deltreenest.1.js
+++ tooltips/deltreenest.1.js
@@ -1,9 +1,9 @@
-funciton deltreenest(major, minor) {
-	
-}
-for (var key in dic) {
+function deltreenest(major, minor){
+   for (var key in dic) {
 											if (key.match(new RegExp(minor +"→(.+)"))) {
 												deltreenest(minor, RegExp.$1);
 											}
 											delete dic[major +"→"+ minor];
 										}
+
+}
