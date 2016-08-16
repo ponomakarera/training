8b8aba7769c1bef6238a82e26d7d8c23cf81d53e
ponomakarera <nidanmawashigeri@gmail.com>
2016-08-16 18:31:02 +0900

    Update and rename addlink6.3.4.js to addlink6.3.5.js

--- addlink/addlink6.3.5.js
+++ addlink/addlink6.3.5.js
@@ -5,12 +5,14 @@ $(function () {
 	var body = {};
 	var url = {};
 	
-	$.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=1&jsonp=?", function (data) {
+	$.when($.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=1&jsonp=?", function (data) {
 			
 			var total_posts = data.response.total_posts;
 			var count = Math.floor(total_posts / 20);
-			
-			$.when(function() {for (var i = 0; i <= count; i++) {
+		console.log("process 1");
+	}),
+	function() {
+		for (var i = 0; i <= count; i++) {
 				$.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
 					for (var i in data.response.posts) {
 						array[listnumber++] = data.response.posts[i].title;
@@ -23,9 +25,13 @@ $(function () {
 						body[data.response.posts[i].title] = linkbody;
 						url[data.response.posts[i].title] = data.response.posts[i].post_url;
 					}
-				});}
-			}).done(function() {
-			console.log(array);
+					console.log("process 2");
+				});
+			}
+			
+	}
+	).done(
+	console.log("process 3");
 		array.sort();
 		array.sort(function(a, b) {return b.length - a.length;});
 		
@@ -57,7 +63,5 @@ $(function () {
 		$(document).ready(function(){
 			simple_tooltip(".textlink","tooltip");
 		});
-			});
-			
-	});
+	);
 });
