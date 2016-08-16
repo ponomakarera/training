297e5dd4c350eca975cb574e4e25941654469974
ponomakarera <nidanmawashigeri@gmail.com>
2016-08-16 17:12:32 +0900

    Update and rename addlink6.1.9.js to addlink6.2.0.js

--- addlink/addlink6.2.0.js
+++ addlink/addlink6.2.0.js
@@ -10,7 +10,7 @@ $(function () {
 			var total_posts = data.response.total_posts;
 			var count = Math.floor(total_posts / 20);
 			
-			$.when(for (var i = 0; i <= count; i++) {
+			$.when(function() {for (var i = 0; i <= count; i++) {
 				$.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
 					for (var i in data.response.posts) {
 						array[listnumber++] = data.response.posts[i].title;
@@ -24,7 +24,7 @@ $(function () {
 						url[data.response.posts[i].title] = data.response.posts[i].post_url;
 					}
 				});
-			}).done(function () {
+			}}).done(function () {
 		array.sort();
 		array.sort(function(a, b) {return b.length - a.length;});
 		
