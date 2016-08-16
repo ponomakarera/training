e5079f37459761cd030237734f95d0e3cc79bb44
ponomakarera <nidanmawashigeri@gmail.com>
2016-08-16 18:34:57 +0900

    Update and rename addlink6.3.6.js to addlink6.3.7.js

--- addlink/addlink6.3.7.js
+++ addlink/addlink6.3.7.js
@@ -4,11 +4,12 @@ $(function () {
 	var listnumber = 0;
 	var body = {};
 	var url = {};
+	var count = 0;
 	
 	$.when($.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=1&jsonp=?", function (data) {
 			
 			var total_posts = data.response.total_posts;
-			var count = Math.floor(total_posts / 20);
+			count = Math.floor(total_posts / 20);
 		console.log("process 1");
 	}),
 	function() {
