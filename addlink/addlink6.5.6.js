0ed3c375a6e4c2f22a571a1748451834ec25deb1
ponomakarera <nidanmawashigeri@gmail.com>
2017-01-15 02:55:59 +0900

    Update and rename addlink6.5.5.js to addlink6.5.6.js

--- addlink/addlink6.5.6.js
+++ addlink/addlink6.5.6.js
@@ -5,13 +5,13 @@ $(function () {
 	var body = {};
 	var url = {};
 	
-	$.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=1&jsonp=?", function (data) {
+	$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=1&jsonp=?", function (data) {
 			
 			var total_posts = data.response.total_posts;
 			var count = Math.floor(total_posts / 20);
 			
 			for (var i = 0; i <= count; i++) {
-				$.getJSON("http://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
+				$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=20&offset="+ i * 20 +"&jsonp=?", function (data) {
 					for (var i in data.response.posts) {
 						array[listnumber++] = data.response.posts[i].title;
 						var linkbody = data.response.posts[i].body;
