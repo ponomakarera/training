eadeeb327c5c4a6d1fb6c9fc6bad51cd43bc5b15
ponomakarera <nidanmawashigeri@gmail.com>
2017-01-21 03:36:05 +0900

    Update and rename ad1.0.10.js to ad1.0.11.js

--- tumblrad/ad1.0.11.js
+++ tumblrad/ad1.0.11.js
@@ -1,12 +1,10 @@
 $(function () {
 
 	var linkurl = [
-	  'https://ponomakarera.tumblr.com/post/137832799790/%E8%80%83%E3%81%88%E3%82%8B%E4%BA%BA',//考える人
-	  'https://ponomakarera.tumblr.com/tagged/%E5%A6%84%E6%83%B3%E9%83%BD%E5%B8%82',//妄想都市
+	  'https://ponomakarera.tumblr.com/post/156130120710/%E5%A6%84%E6%83%B3%E9%83%BD%E5%B8%82',//妄想都市
 	]
 	
 	var image = [
-	  'https://68.media.tumblr.com/ff999e638de6b4d4258970024bf50b1a/tumblr_o5hc04sjOB1t0jrzao1_500.png',
 	  'https://68.media.tumblr.com/a42ff6d3f4ddb6734dec1672db2bb6b1/tumblr_o5xwixDP3z1t0jrzao1_500.png',
 	]
 	
