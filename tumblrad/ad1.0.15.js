85d62d638f7b7753909c73323e8c6ef3002e184f
ponomakarera <nidanmawashigeri@gmail.com>
2017-02-03 04:47:20 +0900

    Update and rename ad1.0.14.js to ad1.0.15.js

--- tumblrad/ad1.0.15.js
+++ tumblrad/ad1.0.15.js
@@ -11,7 +11,7 @@ $(function () {
 	var image = [
 	  'https://68.media.tumblr.com/a42ff6d3f4ddb6734dec1672db2bb6b1/tumblr_o5xwixDP3z1t0jrzao1_500.png',
 	  'https://68.media.tumblr.com/076e815b6c14450bddff046b0333d931/tumblr_ok75pcYIvw1t0jrzao1_500.png',
-	  'https://68.media.tumblr.com/4fe733a7b177152812ab6ae418f3cd1d/tumblr_okq4ixlt7E1t0jrzao1_1280.png',
+	  'https://68.media.tumblr.com/b6acbb548c0408735908365cd6996e55/tumblr_okq4ixlt7E1t0jrzao1_r2_1280.png',
 	  'https://68.media.tumblr.com/3e6ebcfd023e2d8ed70ffe2e5d4d22d4/tumblr_okq9goxYnh1t0jrzao1_500.png',
 	  'https://68.media.tumblr.com/66b3a66a8fef1852876374dda88782dc/tumblr_okqa838NzS1t0jrzao1_500.png',
 	]
