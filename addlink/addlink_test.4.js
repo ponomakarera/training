ae09aac24ff2f10bd9954d658484e13f887c21bf
ponomakarera <nidanmawashigeri@gmail.com>
2017-02-18 18:03:41 +0900

    Update and rename addlink_test.3.js to addlink_test.4.js

--- addlink/addlink_test.4.js
+++ addlink/addlink_test.4.js
@@ -36,6 +36,9 @@ $(function () {
 							}
 							$(this).html(txt);
 						});
+						
+						var addlinkcount = 0;
+						
 						$(".addlink").each(function(){
 							var txt = $(this).html();
 							for (var i = 0; i < array.length; i++) {
@@ -44,14 +47,18 @@ $(function () {
 								}
 								else {
 									if (txt.match("<span id=."+ i +".></span>")) {
-										txt = txt.replace(new RegExp("<span id=."+ i +".></span>"),"<a  class='textlink' id='textlink"+ i +"'; href='"+url[array[i]]+"' style='color:#0645ad '>"+array[i]+"</a>");
+										txt = txt.replace(new RegExp("<span id=."+ i +".></span>"),"<a  class='textlink' id='textlink"+ addlinkcount +"'; href='"+url[array[i]]+"' style='color:#0645ad '>"+array[i]+"</a>");
 										txt = txt.replace(new RegExp("<span id=."+ i +".></span>", "g"),array[i]);
-										$("body").append("<div class='tooltip' id='tooltip"+i+"'><p>"+body[array[i]]+"</p></div>");	
-										simple_tooltip("textlink","tooltip", i);
+										$("body").append("<div class='tooltip' id='tooltip"+addlinkcount+"'><p>"+body[array[i]]+"</p></div>");
+										addlinkcount++;
 									}
 								}
 							}
 							$(this).html(txt);
+						});
+						
+						$(document).ready(function(){
+							simple_tooltip("textlink","tooltip");
 						});
 					}
 				});
