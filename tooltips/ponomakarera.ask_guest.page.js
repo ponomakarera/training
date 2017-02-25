87f957f258d29387754a490e4bafb213b11aba90
ponomakarera <nidanmawashigeri@gmail.com>
2017-02-25 19:05:32 +0900

    Create ponomakarera.ask_guest.page.js

---
+++ tooltips/ponomakarera.ask_guest.page.js
@@ -0,0 +1,9 @@
+$(function () {
+
+	var href = window.location.href ;
+	
+	if (href.match(new RegExp("https://ponomakarera.tumblr.com/ask")) || href.match(new RegExp("https://ponomakarera.tumblr.com/submit"))) {
+		$("span #del").removeClass("addlink");
+	}
+	
+});
