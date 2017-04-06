8c70e80ee27dff93dafc1fabd4469f9aec5a9bfd
ponomakarera <nidanmawashigeri@gmail.com>
2017-04-06 19:10:22 +0900

    Create addlink.8.3.3.js

---
+++ addlink/addlink.8.3.3.js
@@ -0,0 +1,69 @@
+$(function () {
+	
+	var array = new Array();
+	var listnumber = 0;
+	var body = {};
+	var url = {};
+	
+	$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=1&jsonp=?", function (data) {
+			
+			var total_posts = data.response.total_posts;
+			var count = Math.floor(total_posts / 50);
+			
+			for (var i = 0; i <= count; i++) {
+				$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag=%E3%83%A1%E3%83%A2&limit=50&offset="+ i * 50 +"&jsonp=?", function (data) {
+					for (var i in data.response.posts) {
+						array[listnumber++] = data.response.posts[i].title;
+						body[data.response.posts[i].title] = data.response.posts[i].body;
+						url[data.response.posts[i].title] = data.response.posts[i].post_url;
+					}
+					if (listnumber == total_posts) {
+						console.log("sucess!");
+						array.sort();
+						array.sort(function(a, b) {return b.length - a.length;});
+		
+						$(".addlink").each(function(){
+							var txt = $(this).html();
+							var contents = [];
+							while (txt.match(/<linkcancel>(?!<\/linkcancel>)[\s\S]+?<\/linkcancel>/)) {
+								txt = txt.replace(new RegExp(/<linkcancel>(?!<\/linkcancel>)([\s\S]+?)<\/linkcancel>/),"<linkcancel></linkcancel>");
+								contents.push(RegExp.$1);
+							}
+							for (var i = 0; i < array.length; i++) {
+								txt = txt.replace(new RegExp(array[i], "g"),"<span id='"+ i +"'></span>");
+							}
+							for (var i = 0; i < contents.length; i++) {
+								txt = txt.replace(new RegExp("<linkcancel></linkcancel>"),"<linkcancel>"+ contents[i] +"</linkcancel>");
+							}
+							$(this).html(txt);
+						});
+						
+						var addlinkcount = 0;
+						
+						$(".addlink").each(function(){
+							var txt = $(this).html();
+							for (var i = 0; i < array.length; i++) {
+								if (($(this).hasClass(array[i]))) {
+									txt = txt.replace(new RegExp("<span id=."+ i +".></span>", "g"),array[i]);
+								}
+								else {
+									if (txt.match("<span id=."+ i +".></span>")) {
+										txt = txt.replace(new RegExp("<span id=."+ i +".></span>"),"<a  class='textlink' id='textlink"+ addlinkcount +"'; href='"+url[array[i]]+"' style='color:#0645ad '>"+array[i]+"</a>");
+										txt = txt.replace(new RegExp("<span id=."+ i +".></span>", "g"),array[i]);
+										$("body").append("<div class='tooltip' id='tooltip"+addlinkcount+"'><p>"+body[array[i]]+"</p></div>");
+										addlinkcount++;
+									}
+								}
+							}
+							$(this).html(txt);
+						});
+						
+						$(document).ready(function(){
+							simple_tooltip("textlink","tooltip");
+						});
+					}
+				});
+			}
+			
+	});
+});
