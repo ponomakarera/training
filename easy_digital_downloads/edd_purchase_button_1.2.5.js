bc7301e05641e72fba403093828d3fef4a6f7d2b
ponomakarera <nidanmawashigeri@gmail.com>
2017-05-10 00:50:39 +0900

    Update and rename edd_purchase_button_1.2.4.js to edd_purchase_button_1.2.5.js

--- easy_digital_downloads/edd_purchase_button_1.2.5.js
+++ easy_digital_downloads/edd_purchase_button_1.2.5.js
@@ -32,7 +32,7 @@ $(function () {
 	});
 function modal_window(download_id, id, email) {
 	
-	$("#iziModal").append("<p>ご記入したメールアドレス&nbsp;&lt;"+ email +"&gt;&nbsp;に商品のダウンロードリンクを記載したレシートを送信しましたので、ご確認をお願いします。</p>")
+	$("#iziModal").append("<p>ご記入したメールアドレス&nbsp;&lt;"+ email +"&gt;&nbsp;に商品のダウンロードリンクを記載<br>したレシートを送信しましたので、ご確認をお願いします。</p>")
 	$('#iziModal').iziModal({
 		title: '決済が完了しました',
 		headerColor: '#00830c',
