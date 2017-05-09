847749a1dbefa92f44ae3129579996518dcc65b7
ponomakarera <nidanmawashigeri@gmail.com>
2017-05-09 17:10:31 +0900

    Create edd_purchase_button.js

---
+++ easy_digital_downloads/edd_purchase_button.js
@@ -0,0 +1,33 @@
+$(function () {
+
+	jQuery(document).ready(function($) {
+	
+		$('.edd_button').click(function(e) {
+			
+			var download_id = $(this).attr('#download_id');
+			var product_title = $(this).attr('.product_title');
+			var amount = $(this).attr('.amount');
+			
+			StripeCheckout.configure({
+				key: 'pk_test_S0hENx8vOQaCk3UsGTs3W0eC',
+				locale: 'auto',
+				//image: '/square-image.png',
+				token: function(token) {
+
+				},
+				opened: function() {
+
+				},
+				closed: function() {
+
+				}
+			}).open({
+				name: product_title,
+				amount: amount,
+			});
+			
+				return false;
+		
+		});
+	});
+});
