e4e73ee000cf84b8588173b33da9cbd5cbd5174b
ponomakarera <nidanmawashigeri@gmail.com>
2017-05-10 13:16:51 +0900

    Update and rename edd_purchase_button_1.3.1.js to edd_purchase_button_1.3.2.js

--- easy_digital_downloads/edd_purchase_button_1.3.2.js
+++ easy_digital_downloads/edd_purchase_button_1.3.2.js
@@ -33,35 +33,24 @@ $(function () {
 function modal_window(download_id, id, email) {
 
 	$('#iziModal').iziModal({
+		iframe: true,
+		iframeHeight: 100,
+		iframeURL: "https://ponomakarera.com/checkout/purchase-confirmation/"
+ 
+	});
 	
-		onOpening: function(modal){
-		
-			modal.startLoading();
-			$('#iziModal').iziModal({
-				title: '決済が完了しました',
-				headerColor: '#00830c',
-				iframe: true,
-				iframeHeight: 800,
-				iframeURL: "https://ponomakarera.com/checkout/purchase-confirmation/"
-			});
-			$.ajax({
-				url: 'https://ponomakarera.com/checkout/purchase-confirmation/',
-				type: "POST",
-				dataType: "html",
-				data:{download_id: download_id, edd_action: "straight_to_gateway", edd_stripe_token: id, edd_email: email},
-				success: function(data) {
-					modal.stopLoading();
-					$('#iziModal').iziModal('open');
-				},
-				error: function(){
+	$.ajax({
+		url: 'https://ponomakarera.com/checkout/purchase-confirmation/',
+		type: "POST",
+		dataType: "html",
+		data:{download_id: download_id, edd_action: "straight_to_gateway", edd_stripe_token: id, edd_email: email},
+		success: function(data) {
+			$('#iziModal').iziModal('open');
+		},
+		error: function(){
 
-				}
-			});
 		}
 	});
-
-	$('#iziModal').iziModal('open');
-
 	
 	/*$("#iziModal").append("<p>ご記入したメールアドレス&nbsp;&lt;"+ email +"&gt;&nbsp;に商品のダウンロードリンクを記載<br>したレシートを送信しましたので、ご確認をお願いします。</p>")*/
 	
