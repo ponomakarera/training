ddb736854ebba09c8d84be729ebb92bb8616aa77
ponomakarera <nidanmawashigeri@gmail.com>
2017-05-10 13:33:53 +0900

    Update and rename edd_purchase_button_1.3.5.js to edd_purchase_button_1.3.6.js

---
+++ easy_digital_downloads/edd_purchase_button_1.3.6.js
@@ -0,0 +1,56 @@
+$(function () {
+
+	$('.edd_button').click(function(e) {
+	
+		var download_id = $(this).attr('download_id');
+		var product_title = $(this).attr('product_title');
+		var amount = Number($(this).attr('price'));
+			
+		StripeCheckout.configure({
+			key: 'pk_test_S0hENx8vOQaCk3UsGTs3W0eC',
+			locale: 'auto',
+			token: function(token) {
+
+				modal_window(download_id, token.id, token.email);
+
+			},
+			opened: function() {
+
+			},
+			closed: function() {
+
+			}
+		}).open({
+			name: product_title,
+			amount: amount,
+			allowRememberMe: false,
+			currency: 'JPY'
+		});
+			
+		return false;
+		
+	});
+function modal_window(download_id, id, email) {
+
+	$('#iziModal').iziModal({
+		iframe: true,
+		iframeHeight: 400,
+		iframeURL: "https://ponomakarera.com/checkout/purchase-confirmation/"
+ 
+	});
+	
+	$.ajax({
+		url: 'https://ponomakarera.com/checkout/purchase-confirmation/',
+		type: "POST",
+		dataType: "html",
+		data:{download_id: download_id, edd_action: "straight_to_gateway", edd_stripe_token: id, edd_email: email},
+		success: function(data) {
+			$('#iziModal').iziModal('open');
+		},
+		error: function(){
+
+		}
+	});
+	
+}
+});
