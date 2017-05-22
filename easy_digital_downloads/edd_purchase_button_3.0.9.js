af64416e1b853dfd3c1193b35caa1e832ba50d31
ponomakarera <nidanmawashigeri@gmail.com>
2017-05-22 16:47:45 +0900

    Update and rename edd_purchase_button_3.0.8.js to edd_purchase_button_3.0.9.js

--- easy_digital_downloads/edd_purchase_button_3.0.9.js
+++ easy_digital_downloads/edd_purchase_button_3.0.9.js
@@ -42,7 +42,7 @@ $(function () {
 	});
 	
 	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
+	$(".close").onclick = function() {
 		$("#myModal").empty();
 		modal.style.display = "none";
 	}
