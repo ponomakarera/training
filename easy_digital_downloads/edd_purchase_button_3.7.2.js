55b66ea135921c92e109355f1137204a4571ab9f
ponomakarera <nidanmawashigeri@gmail.com>
2017-05-23 01:43:36 +0900

    Update and rename edd_purchase_button_3.7.1.js to edd_purchase_button_3.7.2.js

--- easy_digital_downloads/edd_purchase_button_3.7.2.js
+++ easy_digital_downloads/edd_purchase_button_3.7.2.js
@@ -19,7 +19,7 @@ $(function () {
 				$("#purchaseform").append("<input type='hidden' name='edd_stripe_token' value='" + token.id + "' />");
 				$("#purchaseform").append("<input type='hidden' name='edd_email' value='" + token.email + "' />");
 				modal.style.display = "block";
-				$("#myModal").append("<div class='modal-content'><div class='modal-header'><span style='font-size:20px; font-weight: bold;'>決済が完了しました</span><span class='close'>&times;</span></div><div id='loader-bg'></div><iframe id='iframe-content' name='targetframe' frameborder='0'><iframe></div>");
+				$("#myModal").append("<div class='modal-content'><div class='modal-header'><span style='font-size:20px; color:#FFFFFF; font-weight: bold;'>決済が完了しました</span><span class='close'>&times;</span></div><div id='loader-bg'></div><iframe id='iframe-content' name='targetframe' frameborder='0'><iframe></div>");
 				document.targetform.submit();
 				submitwait();
 				$("#purchaseform").empty();
@@ -50,7 +50,6 @@ $(function () {
 		
 		$("#iframe-content").on("load",function(){
 			try {
-				$('#loader-bg').fadeOut(400);
 				$('#iframe-content').fadeIn(400);
 			}
 			catch(e) {
