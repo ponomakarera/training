0afa51eea35283234cfdbdcc2c6ae35795cb903d
ponomakarera <nidanmawashigeri@gmail.com>
2017-05-22 18:09:46 +0900

    Update and rename edd_purchase_button_3.1.5.js to edd_purchase_button_3.1.6.js

--- easy_digital_downloads/edd_purchase_button_3.1.6.js
+++ easy_digital_downloads/edd_purchase_button_3.1.6.js
@@ -18,7 +18,7 @@ $(function () {
 				$("#purchaseform").append('<input type="hidden" name="edd_action" value="straight_to_gateway">');
 				$("#purchaseform").append("<input type='hidden' name='edd_stripe_token' value='" + token.id + "' />");
 				$("#purchaseform").append("<input type='hidden' name='edd_email' value='" + token.email + "' />");
-				$("#myModal").append("<iframe class='modal-content' name='targetframe'><span class='close'>&times;</span><iframe>");
+				$("#myModal").append("<iframe src='http://www.pixelimage.jp/blog/images_loading_gif/icon_loader_a_ww_01_s1.gif' class='modal-content' name='targetframe'><span class='close'>&times;</span><iframe>");
 				document.targetform.submit();
 				$("#purchaseform").empty();
 				
