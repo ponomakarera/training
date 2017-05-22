961d257bfa544094f40a26bd148991de3552b29d
ponomakarera <nidanmawashigeri@gmail.com>
2017-05-22 15:46:38 +0900

    Update and rename edd_purchase_button_3.0.2.js to edd_purchase_button_3.0.3.js

---
+++ easy_digital_downloads/edd_purchase_button_3.0.3.js
@@ -0,0 +1,39 @@
+$(function () {
+
+	$('.edd_button').click(function(e) {
+	
+		var download_id = $(this).attr('download_id');
+		var product_title = $(this).attr('product_title');
+		var amount = Number($(this).attr('price'));
+		
+		StripeCheckout.configure({
+			key: 'pk_test_S0hENx8vOQaCk3UsGTs3W0eC',
+			locale: 'auto',
+			token: function(token) {
+				
+				document.targetform.append("<input type='hidden' name='download_id' value='" + download_id + "'>");
+				document.targetform.append('<input type="hidden" name="edd_action" value="straight_to_gateway">');
+				document.targetform.append("<input type='hidden' name='edd_stripe_token' value='" + token.id + "' />");
+				document.targetform.append("<input type='hidden' name='edd_email' value='" + token.email + "' />");
+				$("body").append("<iframe name='targetframe'><iframe>");
+				document.targetform.submit();
+				document.targetform.empty();
+				
+			},
+			opened: function() {
+
+			},
+			closed: function() {
+
+			}
+		}).open({
+			name: product_title,
+			amount: amount,
+			allowRememberMe: false,
+			currency: 'JPY'
+		});
+			
+		return false;
+		
+	});
+});
