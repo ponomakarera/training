2ced2bf306b7279557185b6ce9980e28e1b2cc0f
ponomakarera <nidanmawashigeri@gmail.com>
2017-05-22 20:33:39 +0900

    Update and rename edd_purchase_button_3.3.2.js to edd_purchase_button_3.3.3.js

--- easy_digital_downloads/edd_purchase_button_3.3.3.js
+++ easy_digital_downloads/edd_purchase_button_3.3.3.js
@@ -1,7 +1,7 @@
 $(function () {
 
 	var modal = document.getElementById('myModal');
-	var span = document.getElementById("close");
+	var span = document.getElementsByClassName("close");
 	
 	$('.edd_button').click(function(e) {
 	
@@ -20,7 +20,7 @@ $(function () {
 				$("#purchaseform").append("<input type='hidden' name='edd_stripe_token' value='" + token.id + "' />");
 				$("#purchaseform").append("<input type='hidden' name='edd_email' value='" + token.email + "' />");
 				modal.style.display = "block";
-				$("#myModal").append("<div class='modal-content'><div class='modal-header'><span style='font-size:20px; font-weight: bold;'>決済が完了しました</span><span id='close'>&times;</span></div><iframe name='targetframe' frameborder='0'><iframe></div>");
+				$("#myModal").append("<div class='modal-content'><div class='modal-header'><span style='font-size:20px; font-weight: bold;'>決済が完了しました</span><span class='close'>&times;</span></div><iframe name='targetframe' frameborder='0'><iframe></div>");
 				document.targetform.submit();
 				$("#purchaseform").empty();
 				
@@ -50,7 +50,10 @@ $(function () {
 	
 	// When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
-		if (event.target == modal) {
+		if (event.target.className == "modal-content") {
+			return;
+		}
+		else if (event.target.className == "modal") {
 			$("#myModal").empty();
 			modal.style.display = "none";
 		}
