ad0b14f5079f181102e5d9e49790513ee5fa30c9
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-02 11:55:56 +0900

    Update and rename tooltips.test.14.js to tooltips.test.15.js

--- tooltips/tooltips.test.15.js
+++ tooltips/tooltips.test.15.js
@@ -3,19 +3,17 @@ function simple_tooltip(target, reference){
 	var my_tooltip;
 	var tooltip_exist = {};
 	
-	$("."+target).mouseover(function() {
+	TEST: $("."+target).mouseover(function() {
 		
 		var i = $(this).attr('post_id');
 		if (!tooltip_exist[i]) {
 			$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&id="+ i +"&jsonp=?", function (data) {
 			
 				$("body").append("<div class='tooltip' id='"+ reference+ i +"'><p>"+ data.response.posts[0].body +"</p></div>"); tooltip_exist[i] = true;
+				continue TEST;
 				console.log("1");
-				my_tooltip = $("#"+reference+i);
-				my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
 				/*my_tooltip = $("#"+reference+i);
-				my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
-				$("."+target).mousemove(function(kmouse) {});*/
+				my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);*/
 			
 			});	
 		}
