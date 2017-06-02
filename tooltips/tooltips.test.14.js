2a0b3e17e59cbd9f61fbecb9b442d38b48bcb334
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-02 11:42:49 +0900

    Update and rename tooltips.test.13.js to tooltips.test.14.js

--- tooltips/tooltips.test.14.js
+++ tooltips/tooltips.test.14.js
@@ -11,6 +11,8 @@ function simple_tooltip(target, reference){
 			
 				$("body").append("<div class='tooltip' id='"+ reference+ i +"'><p>"+ data.response.posts[0].body +"</p></div>"); tooltip_exist[i] = true;
 				console.log("1");
+				my_tooltip = $("#"+reference+i);
+				my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
 				/*my_tooltip = $("#"+reference+i);
 				my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
 				$("."+target).mousemove(function(kmouse) {});*/
