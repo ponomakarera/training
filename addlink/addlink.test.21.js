bcb750da61cef393adee95ee18fa612bc4bf50f1
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-02 09:35:21 +0900

    Update and rename addlink.test.20.js to addlink.test.21.js

--- addlink/addlink.test.21.js
+++ addlink/addlink.test.21.js
@@ -1,7 +1,12 @@
 (function () {
 	
+	var array = {
+	
+	'あ': '149175896520', 'とは': '148474924130',
+	
+	};
+	
 	var target = document.getElementsByClassName('addlink');
-	var array = {'あ': '149175896520', 'とは': '148474924130'};
 	
 	for (var i = 0; i < target.length; i++){
 		var txt = target[i].innerHTML;
@@ -16,19 +21,18 @@
 		for (var j = 0; j < contents.length; j++) {
 			txt = txt.replace(new RegExp("<linkcancel></linkcancel>"),"<linkcancel>"+ contents[j] +"</linkcancel>");
 		}
-		console.log(target[i].className);
 		for (var key in array) {
 			if ((target[i].className +'').indexOf(key) >= 0) {
 				txt = txt.replace(new RegExp("<span id=."+ array[key] +".></span>", "g"),key);
 			}
 			else {
 				if (txt.match("<span id=."+ array[key] +".></span>")) {
-					txt = txt.replace(new RegExp("<span id=."+ array[key] +".></span>"),"<a  class='textlink' href='https://ponomakarera.tumblr.com/post/"+ array[key] +"' style='color:#0645ad' >"+ key +"</a>");
+					txt = txt.replace(new RegExp("<span id=."+ array[key] +".></span>"),"<a  class='textlink' href='https://ponomakarera.tumblr.com/post/"+ array[key] +"/"+ key +"' style='color:#0645ad' post_id='"+ array[key] +"'>"+ key +"</a>");
 					txt = txt.replace(new RegExp("<span id=."+ array[key] +".></span>", "g"),key);
-					/*if (!tooltip_exist[array[i]]) {$("body").append("<div class='tooltip' id='tooltip"+ i +"'><p>"+ body[array[i]] +"</p></div>"); tooltip_exist[array[i]] = true;}*/
 				}
 			}
 		}
 		target[i].innerHTML = txt;
 	}
+	//$(document).ready(function(){ simple_tooltip("textlink","tooltip"); });
 })();
