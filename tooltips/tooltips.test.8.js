afa2444d7eb340ac83910059e29f5bebb27b8484
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-02 11:12:34 +0900

    Update and rename tooltips.test.7.js to tooltips.test.8.js

--- tooltips/tooltips.test.8.js
+++ tooltips/tooltips.test.8.js
@@ -1,7 +1,8 @@
-function simple_tooltip(target, reference, tooltip_exist = {}){
+function simple_tooltip(target, reference){
 
 	var my_tooltip;
-		
+	var tooltip_exist = {};
+	
 	$("."+target).mouseover(function() {
 		
 		var i = $(this).attr('post_id');
@@ -9,7 +10,8 @@ function simple_tooltip(target, reference, tooltip_exist = {}){
 			$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&id="+ i +"&jsonp=?", function (data) {
 			
 				$("body").append("<div class='tooltip' id='"+ reference+ i +"'><p>"+ data.response.posts[0].body +"</p></div>"); tooltip_exist[i] = true;
-				simple_tooltip("textlink","tooltip", tooltip_exist);
+				$("#"+reference+i).css({opacity:0.8, display:"none"});
+				//simple_tooltip("textlink","tooltip", tooltip_exist);
 			
 			});	
 		}
