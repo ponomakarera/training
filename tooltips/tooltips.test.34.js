e0772a3b966122970671a5058c772e6720a689ec
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-02 19:18:10 +0900

    Update and rename tooltips.test.33.js to tooltips.test.34.js

--- tooltips/tooltips.test.34.js
+++ tooltips/tooltips.test.34.js
@@ -6,15 +6,14 @@ function simple_tooltip(target, reference){
 	$("."+target).mouseover(function() {
 		var i = $(this).attr('post_id');
 		var classname = $(this);
-		if(!matched_word[i]) {
 			console.time('timer1');
 			$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&id="+ i +"&jsonp=?", function (data) {
 				$("body").append("<div class='tooltip' id='"+ reference+ i +"'><p>"+ data.response.posts[0].body +"</p></div>"); 
 				matched_word[i] = true;
 				$(classname).addClass("textlink").removeClass(target);
 				console.timeEnd('timer1');
+				simple_tooltip("addtooltip", "tooltip");
 			});
-		}
 	});
 	
 	$(".textlink").mouseover(function() {
