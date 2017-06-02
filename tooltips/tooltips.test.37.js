f111918fa6c3f6cfbd5c989952889c6531afa656
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-02 19:34:41 +0900

    Update and rename tooltips.test.36.js to tooltips.test.37.js

--- tooltips/tooltips.test.37.js
+++ tooltips/tooltips.test.37.js
@@ -9,9 +9,11 @@ function simple_tooltip(target, reference){
 		var classname = $(this);
 		
 		if(!matched_word[i]) {
+			console.time('timer1');
 			$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&id="+ i +"&jsonp=?", function (data) {
 				$("body").append("<div class='tooltip' id='"+ reference+ i +"'><p>"+ data.response.posts[0].body +"</p></div>"); 
 				matched_word[i] = true;
+				console.timeEnd('timer1');
 				$(classname).trigger("mouseover");
 			});
 		}
