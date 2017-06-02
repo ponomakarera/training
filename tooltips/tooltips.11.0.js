1883dbd74fba530dab4ed22c610bef35eeb8fa78
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-03 01:11:19 +0900

    Update and rename tooltips.test.77.js to tooltips.11.0.js

--- tooltips/tooltips.11.0.js
+++ tooltips/tooltips.11.0.js
@@ -9,10 +9,8 @@ function simple_tooltip(target, reference){
 		
 		if(!matched_word[i]) {
 			matched_word[i] = true;
-			console.time('timer1');
 			$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&id="+ i +"&jsonp=?", function (data) {
 				$("body").append("<div class='tooltip' id='"+ reference+ i +"'><p>"+ data.response.posts[0].body +"</p></div>"); 
-				console.timeEnd('timer1');
 				my_tooltip = $("#"+reference+i);
 				my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
 				var border_top = $(window).scrollTop(); 
