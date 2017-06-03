81db7fc93b0f403b9c9685c647426e770122080e
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-03 10:35:28 +0900

    Update and rename tooltips.11.0.js to tooltips.11.1.js

--- tooltips/tooltips.11.1.js
+++ tooltips/tooltips.11.1.js
@@ -3,7 +3,7 @@ function simple_tooltip(target, reference){
 	var my_tooltip;
 	var matched_word = {};
 	
-	$("."+target).mouseover(function(kmouse) {
+	$("."+target).mouseover(function() {
 	
 		var i = $(this).attr('id');
 		
@@ -11,30 +11,35 @@ function simple_tooltip(target, reference){
 			matched_word[i] = true;
 			$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&id="+ i +"&jsonp=?", function (data) {
 				$("body").append("<div class='tooltip' id='"+ reference+ i +"'><p>"+ data.response.posts[0].body +"</p></div>"); 
-				my_tooltip = $("#"+reference+i);
-				my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
-				var border_top = $(window).scrollTop(); 
-				var border_right = $(window).width();
-				var left_pos;
-				var top_pos;
-				var right_pos;
-				var offset = 20;
-				if(border_right - (offset *2) >= my_tooltip.width() + kmouse.pageX){
-					left_pos = kmouse.pageX+offset;
-				}
-				else if (border_right-my_tooltip.width()-offset < 0){
-					left_pos = 0;
-				}
-				else {
-					left_pos = border_right-my_tooltip.width()-offset;
-				}
+				if($(':hover').is("#"+ i)) {
+					my_tooltip = $("#"+reference+i);
+					my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
+					
+					$(window).mousemove(function(kmouse) {
+						var border_top = $(window).scrollTop(); 
+						var border_right = $(window).width();
+						var left_pos;
+						var top_pos;
+						var right_pos;
+						var offset = 20;
+						if(border_right - (offset *2) >= my_tooltip.width() + kmouse.pageX){
+							left_pos = kmouse.pageX+offset;
+						}
+						else if (border_right-my_tooltip.width()-offset < 0){
+							left_pos = 0;
+						}
+						else {
+							left_pos = border_right-my_tooltip.width()-offset;
+						}
 				
-				if(border_top + (offset *2)>= kmouse.pageY - my_tooltip.height()){
-					top_pos = border_top +offset;
-				} else{
-					top_pos = kmouse.pageY-my_tooltip.height()-offset;
-				}
-				if($(':hover').is("#"+ i)) {my_tooltip.css({left:left_pos, top:top_pos});}				
+						if(border_top + (offset *2)>= kmouse.pageY - my_tooltip.height()){
+							top_pos = border_top +offset;
+						} else{
+							top_pos = kmouse.pageY-my_tooltip.height()-offset;
+						}
+						my_tooltip.css({left:left_pos, top:top_pos});
+					});
+				}		
 			});
 		}
 		
