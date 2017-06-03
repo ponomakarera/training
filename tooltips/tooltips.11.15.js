5664f9eac85ef0675a087875aa0f3c78155ae75f
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-04 02:03:50 +0900

    Update and rename tooltips.11.14.js to tooltips.11.15.js

--- tooltips/tooltips.11.15.js
+++ tooltips/tooltips.11.15.js
@@ -12,7 +12,7 @@ function simple_tooltip(target, reference){
 			tooltip_exist[i] = true;
 			$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&id="+ i +"&jsonp=?", function (data) {
 				$("body").append("<div class='tooltip' id='"+ reference+ i +"'><p>"+ data.response.posts[0].body +"</p></div>"); 
-				if($(':hover').is("#"+ i)) {
+				if($(':hover').("#"+ i).length) {
 					my_tooltip = $("#"+reference+i);
 					my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
 					var border_top = $(window).scrollTop(); 
