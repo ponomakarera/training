cafc6122059e8b6e20e9eb6968e2d72c8ab78869
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-04 17:44:06 +0900

    Create delladdlink.js

---
+++ addlink/delladdlink.js
@@ -0,0 +1,27 @@
+(function () {
+
+	
+	var currentScript = document.currentScript || (function() {
+		var nodeList = document.getElementsByTagName('script')
+		return nodeList.item(nodeList.length - 1)
+	}())	
+	
+	var closestByClass = function(element, classname) {
+		// Traverse the DOM up with a while loop
+		while (element.className != classname) {
+			// Increment the loop to the parent node
+			element = element.parentNode;
+			if (!element) {
+				return null;
+			}
+		}
+		// At this point, the while loop has stopped and `element` represents the element that has
+		// the class you specified in the second parameter of the function `classname`
+
+		// Then return the matched element
+		return element;
+	}
+	
+	clasestByClass(currentScript, "addlink").classList.toggle('addlink');
+	
+})();
