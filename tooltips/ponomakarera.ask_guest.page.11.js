a2ecf54da4fda8055e11c1b8005ac370a851fdc4
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-04 14:03:32 +0900

    Update and rename ponomakarera.ask_guest.page.10.js to ponomakarera.ask_guest.page.11.js

--- tooltips/ponomakarera.ask_guest.page.11.js
+++ tooltips/ponomakarera.ask_guest.page.11.js
@@ -3,7 +3,7 @@
 	var href = window.location.href ;
 	
 	if (href.match(new RegExp("https://ponomakarera.tumblr.com/ask")) || href.match(new RegExp("https://ponomakarera.tumblr.com/submit"))) {
-		$(function () { $("script").closest(".addlink").removeClass("addlink"); });
+		$(function () { $(".addlink").removeClass("addlink"); });
 	}
 	
 })();
