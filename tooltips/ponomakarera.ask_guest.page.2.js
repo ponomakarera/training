30a0b2c8ba36e9f1842f5aa53e770ba160cef2d2
ponomakarera <nidanmawashigeri@gmail.com>
2017-06-04 19:34:31 +0900

    Delete ponomakarera.ask_guest.page.2.js

--- tooltips/ponomakarera.ask_guest.page.2.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-	var href = window.location.href ;
-	
-	if (href.match(new RegExp("https://ponomakarera.tumblr.com/ask")) || href.match(new RegExp("https://ponomakarera.tumblr.com/submit"))) {
-		$("span,#del").removeClass("addlink");
-	}
-	
-});
