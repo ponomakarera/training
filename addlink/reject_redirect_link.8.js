5cd436f8b596cf253c4922e1e9327bb319445d2a
ponomakarera <nidanmawashigeri@gmail.com>
2017-07-11 21:47:40 +0900

    Update and rename reject_redirect_link.7.js to reject_redirect_link.8.js

--- addlink/reject_redirect_link.8.js
+++ addlink/reject_redirect_link.8.js
@@ -1,14 +1,11 @@
 $(function () {
 
-	var redilink = 't.umblr.com/redirect?z=';
     $("a").each(function() {
 	
 		//get the original URL
 		var theURL = $(this).attr('href');
 		
 		if (theURL != null && theURL.match(/t.umblr.com\/redirect\?z\=/)) {
-		
-			console.log(theURL);
 		
 			//split at the equals sign
 			var one = theURL.split('=');
