509cb908bb0064dd086f5ac30b45af9231beb77f
ponomakarera <nidanmawashigeri@gmail.com>
2017-07-11 21:06:31 +0900

    Create Reject_redirect_link.js

---
+++ addlink/Reject_redirect_link.js
@@ -0,0 +1,20 @@
+$(function () {
+    $("a").each(function() {
+	
+		//get the original URL
+		var theURL = $(this).attr('href');
+		
+		//split at the equals sign
+        var one = theURL.split('=');
+		
+		//split again at the amperstand
+        var two = one[1].split('&')
+		
+		//get rif of the URL encoding
+        var decodedURL = decodeURIComponent(two[0])
+		
+		//put decoded URL in the href attribute
+        $(this).attr('href', decodedURL)
+    
+	});
+});
