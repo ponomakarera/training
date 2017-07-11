8c651ab98f0e87750ca800e1bcb0d963962c1f01
ponomakarera <nidanmawashigeri@gmail.com>
2017-07-11 21:12:13 +0900

    Update and rename Reject_redirect_link.js to reject_redirect_link.1.js

--- addlink/reject_redirect_link.1.js
+++ addlink/reject_redirect_link.1.js
@@ -8,13 +8,13 @@ $(function () {
         var one = theURL.split('=');
 		
 		//split again at the amperstand
-        var two = one[1].split('&')
+        var two = one[1].split('&');
 		
 		//get rif of the URL encoding
-        var decodedURL = decodeURIComponent(two[0])
+        var decodedURL = decodeURIComponent(two[0]);
 		
 		//put decoded URL in the href attribute
-        $(this).attr('href', decodedURL)
+        $(this).attr('href', decodedURL);
     
 	});
 });
