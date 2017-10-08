af54153470a551c93a0cb1c38269d469fd1a9409
ponomakarera <nidanmawashigeri@gmail.com>
2017-10-09 01:49:54 +0900

    Update and rename working_link.0.21.js to working_link.0.22.js

--- tooltips/working_link.0.22.js
+++ tooltips/working_link.0.22.js
@@ -14,7 +14,7 @@ $(function () {
 	
 	for (i = 0; i < working.length; i++) {
 	
-		$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag="+ working[listnumber] +"&limit=1&jsonp=?", function (data) {
+		$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag="+ working[i] +"&limit=1&jsonp=?", function (data) {
 			date[listnumber] = data.response.posts[0].date;
 			datework[data.response.posts[0].date] = working[listnumber++];
 			
