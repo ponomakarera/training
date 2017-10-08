fcfed91c420cc8af6d4fbe5d0a5c7decfe5b1d3c
ponomakarera <nidanmawashigeri@gmail.com>
2017-10-08 23:41:40 +0900

    Update and rename working_link.0.2.js to working_link.0.3.js

--- tooltips/working_link.0.3.js
+++ tooltips/working_link.0.3.js
@@ -14,16 +14,18 @@ $(function () {
 		$.getJSON("https://api.tumblr.com/v2/blog/ponomakarera.tumblr.com/posts/?api_key=1Uw1n0Yvp6uylFWhR8AyhgmPTgAlvItyeOFK6XKuYcMYiygM6V&tag="+ working[i] +"&limit=1&jsonp=?", function (data) {
 			date[i] = data.response.posts[0].date;
 			datework[data.response.posts[0].date] = working[i];
+			
+			if (i == working.length) {
+				if (working.length > 1) {date.sort(comparedate);}
+	
+				for (var i in date) {
+					$("body").append("<a href='https://ponomakarera.tumblr.com/tagged/"+ datework[date[i]] +"' style='color:#00830c; text-decoration: none;'>"+ datework[date[i]] +"</a><br><span style='color:#666; font-size: 12px;'>"+ date[i].replace(new RegExp(' GMT'),'') +"</span><br><br>");
+				}
+			}
+			
 		});
 	
 	}
-	
-	if (working.length > 1) {date.sort(comparedate);}
-	
-	for (var i in date) {
-		$(".side-tags").append("<a href='https://ponomakarera.tumblr.com/tagged/"+ datework[date[i]] +"' style='color:#00830c; text-decoration: none;'>"+ datework[date[i]] +"</a><br><span style='color:#666; font-size: 12px;'>"+ date[i].replace(new RegExp(' GMT'),'') +"</span><br><br>");
-	}
-	
 	
 	function comparedate (a, b) {
 		
@@ -34,7 +36,7 @@ $(function () {
 		var aminute
 		var asecond
 		if (a.match(/(.+)-(.+)-(.+) (.+):(.+):(.+)/)) {
-			ayer = RegExp.$1;
+			ayear = RegExp.$1;
 			amonth = RegExp.$2;
 			aday = RegExp.$3;
 			ahour = RegExp.$4;
@@ -49,7 +51,7 @@ $(function () {
 		var bminute
 		var bsecond
 		if (b.match(/(.+)-(.+)-(.+) (.+):(.+):(.+)/)) {
-			byer = RegExp.$1;
+			byear = RegExp.$1;
 			bmonth = RegExp.$2;
 			bday = RegExp.$3;
 			bhour = RegExp.$4;
@@ -57,7 +59,7 @@ $(function () {
 			bsecond = RegExp.$6;
 		}
 		
-		if (ayer < byear) {return 1;}
+		if (ayear < byear) {return 1;}
 		if (amonth < bmonth) {return 1;}
 		if (aday < bday) {return 1;}
 		if (ahour < bhour) {return 1;}
