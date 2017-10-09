be9ed72416d3dce594a9ba61f636c311ed4620c6
ponomakarera <nidanmawashigeri@gmail.com>
2017-10-10 06:29:11 +0900

    Update and rename working_link.0.59.js to working_link.0.60.js

--- tooltips/working_link.0.60.js
+++ tooltips/working_link.0.60.js
@@ -92,7 +92,6 @@ $(function () {
 		if (date.match(/\d{4}-(\d{2})-(\d{2})/)) {
 			if (Number(RegExp.$1) < 10) {
 				date = date.replace(new RegExp("-"+ RegExp.$1 +"-"),"-"+ Number(RegExp.$1) +"-");
-				console.log(date.replace(new RegExp("-"+ RegExp.$1 +"-"),"-"+ Number(RegExp.$1) +"-"));
 			}
 			if (Number(RegExp.$2) < 10) {
 				date = date.replace(new RegExp("-"+ RegExp.$2 +" "),"-"+ Number(RegExp.$2) +" ");
