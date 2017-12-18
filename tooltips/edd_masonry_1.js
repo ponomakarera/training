775ae0d03988ab707ed1539eac7bef255441f9c7
ponomakarera <nidanmawashigeri@gmail.com>
2017-12-18 19:07:08 +0900

    Update and rename edd_masonry.js to edd_masonry_1.js

--- tooltips/edd_masonry_1.js
+++ tooltips/edd_masonry_1.js
@@ -1,7 +1,6 @@
 jQuery(window).load(function() {
     jQuery('.edd_downloads_list').masonry({
         itemSelector: '.edd_download',
-        columnWidth: 300,
         isAnimated:true,
         isFitWidth: true
      });
