c3c1f2d35549ed9c4d715dee4a48b69a55db5f31
ponomakarera <nidanmawashigeri@gmail.com>
2018-08-31 22:08:03 +0900

    Update and rename sitebanner-ads-0.5.js to sitebanner-koukoku-0.5.js

--- sitebanner/sitebanner-koukoku-0.5.js
+++ sitebanner/sitebanner-koukoku-0.5.js
@@ -39,7 +39,7 @@
 	var rand = Math.floor( Math.random() * linkurl.length );
 	
 	document.getElementsByClassName("antenna").innerHTML = '<a href="'+ linkurl[rand] +'" class="logo" target="_blank">
-<div class="slide-img" style="background-image: url(&quot;'+ image[rand] +'&quot;);"'></div>
-</a>;
+<div class="slide-img" style="background-image: url(&quot;'+ image[rand] +'&quot;);"></div>
+</a>';
 	}
 })();
