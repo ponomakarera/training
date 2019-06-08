9f79a911d9ac294bdc4c050707f0b38179f2d4e6
ponomakarera <nidanmawashigeri@gmail.com>
2019-06-09 02:44:31 +0900

    Update productslink.1.0.21.js

--- tumblrad/productslink.1.0.21.js
+++ tumblrad/productslink.1.0.21.js
@@ -17,7 +17,8 @@
           'https://ponomakarera.social/post/176382764625/tumblr%E3%83%86%E3%83%BC%E3%83%9E-photography',//Tumblrテーマ – Photography
           'https://ponomakarera.social/post/176949685825/tumblr%E3%83%86%E3%83%BC%E3%83%9E-photography-2',//Tumblrテーマ – Photography 2
 	  'https://ponomakarera.social/post/178062686225/tumblr%E3%83%86%E3%83%BC%E3%83%9E-linkit',//Tumblrテーマ – Linkit
-	  'https://ponomakarera.social/post/178538050475/the-war-for-science-v-1',//Wars for Science (v 1)
+	  'https://ponomakarera.social/post/178538050475/wars-for-science-v-1',//Wars for Science (v 1)
+	  'https://ponomakarera.social/post/185454093865/glory-of-a-city-v-1',//Glory of a City (v 1)
 	]
 	
 	//image
@@ -38,6 +39,7 @@
 	  'https://78.media.tumblr.com/c8f53e248fa3e1380aee2e2200c9f0e9/tumblr_pdeh7reJL81t0jrzao1_1280.png',//Tumblrテーマ – Photography 2
 	  'https://78.media.tumblr.com/af83c5b752adaa1f05e9eaeccadfa8bb/tumblr_pf0yfa9TLO1t0jrzao1_1280.png',//Tumblrテーマ – Linkit
 	  'https://66.media.tumblr.com/f18b6fe85f94b210a9f37f8fccecf32c/tumblr_pfrgygXpUT1t0jrzao1_r1_1280.jpg',//Wars for Science (v 1)
+	  'https://66.media.tumblr.com/46b469f0646e0f3ddabfb218e4970d13/tumblr_pssi8eJiEl1t0jrzao1_1280.jpg',//Glory of a City (v 1)
 	]
 	
 	var rand = Math.floor( Math.random() * linkurl.length );
